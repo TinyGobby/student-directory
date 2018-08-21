a9e7b042dc152f49e4564a94e84e49704b969edf
Patrick Harris <patricknpharris@gmail.com>
2018-08-21 11:53:42 +0100

    now saves output to file

--- interactive_directory.rb
+++ interactive_directory.rb
@@ -22,21 +22,15 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  # get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
     @students << {name: name, cohort: month}
     puts "Now we have #{@students.count} students"
-    # now get another name from the user
     name = gets.chomp
     if name.empty?
       break
     end
   end
-  # return the array of students
   @students
 end
 
@@ -64,6 +58,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -73,12 +68,26 @@ def show_students
   print_footer
 end
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def process(selection)
   case selection
   when "1"
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
